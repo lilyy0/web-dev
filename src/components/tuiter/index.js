0e55dc9475ed9a3afa1b08d37142eb7371bde013
Lily Hughes <lilyh@Lilys-MacBook-Pro.local>
2022-03-17 19:36:47 -0400

    failed build

--- src/components/tuiter/index.js
+++ src/components/tuiter/index.js
@@ -1,8 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import NavigationSidebar from "./NavigationSidebar";
-import WhoToFollowList from "./WhoToFollowList";
-import PostSummaryList from "./PostSummaryList";
 import ExploreScreen from "./ExploreScreen";
 
 const Tuiter = () => {
